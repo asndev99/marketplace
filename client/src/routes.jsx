7fa9e18aae4a31b0e63bcb3f5a9404e1ab632c45
asndev <asndev99@gmail.com>
2025-10-31 18:32:56 +0500

    frontend initialization

---
+++ client/src/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './Layout/MainLayout'
+import Home from './Pages/Home'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path='/' element={<Home />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default AppRoutes
